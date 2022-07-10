3c8aa00d3e5907cec6455644f49ee7a3128402b7
Grotawred <grishavlasko200@gmail.com>
2022-07-10 20:40:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        String Line = "зараз корок ротатор дід ок";
+        String[] arr = new String[20];
+        String[] reverseArray = new String[20];
+        String[] words = Line.split(" ");
+        for ( String ignored : words ) {
+            arr = words;
+        }
+        for(int i = 0; i< arr.length;i++){
+            String reversedString = new StringBuffer(arr[i]).reverse().toString();
+            boolean bool = true;
+            while (bool) {
+                if (Objects.equals(arr[i], reversedString)) {
+                    reverseArray[i] = reversedString;
+                    break;
+                }
+                bool = false;
+            }
+        }
+        int i = 0;
+        int min = i;
+        for(int j = i;j< arr.length;j++){
+            if(arr[min].length()<arr[j].length()) {
+                min = j;
+            }
+        }
+        System.out.println(arr[min]);
+    }
+}
